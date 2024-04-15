c0b059fea3c36eacb2df85442a9fd1ba713ee6fc
Satyajeet Das Chowdhury <51711970+satyajeetdc@users.noreply.github.com>
2024-04-15 15:26:59 +0000

    more datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,16 +2,36 @@
 
 // alert("hello");   // We are using node js, not browser
 
-let name = "Satyajeet";   // string
-let age = 21;             // number
-let isLoggedIn = false;   // boolean
+let name = "Satyajeet"; // string
+let age = 21; // number
+let isLoggedIn = false; // boolean
 
 // null --> standalone value, shows the variable is empty
 // undefined --> no value is defined
 // symbol --> used for finding uniqueness, used in React, figma, etc...
 
-// object 
+// object
 
-console.log(typeof null);  // OBJECT
-console.log(typeof undefined);  // undefined
+// console.log(typeof null); // OBJECT
+// console.log(typeof undefined); // undefined
 
+// const id = Symbol("123");
+// const nextId = Symbol("123");
+// console.log(id === nextId); // false
+// console.log(id);
+// console.log(nextId);
+
+// Array, Objects, fucntion
+
+const arr = [1, 2, 3];
+
+let myObject = {
+  name: "satyajeet",
+  age: 25,
+};
+
+const myFunc = function () {
+  console.log("Hello World!");
+};
+
+console.table([typeof arr, typeof myObject, typeof myFunc]);

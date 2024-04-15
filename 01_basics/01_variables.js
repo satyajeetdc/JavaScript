ccef678b11f0b731f7e1c3ac0fbe70d131aedc45
Satyajeet Das Chowdhury <51711970+satyajeetdc@users.noreply.github.com>
2024-04-15 13:47:15 +0000

    variables and data-types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553;
+let accountEmail = "satyajeetdc@gmail.com";
+var accountPassword = "password";
+accountCity = "Jaipur";    // DONT
+let accountState;
+
+// accountId = 2;      // NOT ALLOWED
+accountEmail = "sample@sample.com";  // can be edited
+accountPassword = "12345";   // can be edited
+accountCity = "Lucknow";   // can be edited    // DONT
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // Treat all JS code as in newer version
+
+// alert("hello");   // We are using node js, not browser
+
+let name = "Satyajeet";   // string
+let age = 21;             // number
+let isLoggedIn = false;   // boolean
+
+// null --> standalone value, shows the variable is empty
+// undefined --> no value is defined
+// symbol --> used for finding uniqueness, used in React, figma, etc...
+
+// object 
+
+console.log(typeof null);  // OBJECT
+console.log(typeof undefined);  // undefined
+

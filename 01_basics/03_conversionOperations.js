46fb3a59a6f58a7fcc38eab68e8919f12446e9b9
Satyajeet Das Chowdhury <51711970+satyajeetdc@users.noreply.github.com>
2024-04-15 14:36:40 +0000

    operators

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,26 +1,57 @@
-let score = "33";
-console.log(typeof(score));
-console.log(typeof score);
+// let score = "33";
+// console.log(typeof(score));
+// console.log(typeof score);
 
-let value = Number(score);
-console.log(typeof value);
+// let value = Number(score);
+// console.log(typeof value);
 
-let newScore= "33a";
-let newValue = Number(newScore); // NaN
-console.log(typeof newValue);
-console.log(newValue);   // NaN
+// let newScore= "33a";
+// let newValue = Number(newScore); // NaN
+// console.log(typeof newValue);
+// console.log(newValue);   // NaN
 
-let a = null;
-console.log(a);
-console.log(Number(a));   // 0
+// let a = null;
+// console.log(a);
+// console.log(Number(a));   // 0
 
-let b = false;
-console.log(b);
-console.log(Number(b));   // false -> 0, true -> 1
+// let b = false;
+// console.log(b);
+// console.log(Number(b));   // false -> 0, true -> 1
 
-let isLoggedIn = true;
-console.log(isLoggedIn);
-console.log(typeof Number(isLoggedIn));
+// let isLoggedIn = true;
+// console.log(isLoggedIn);
+// console.log(typeof Number(isLoggedIn));
+
+/* ------------------- OPERATIONS ------------------- */
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
 
 
+// let str1 = "Satyajeet";
+// let str2 = " Chowdhury";
+// let str = str1 + str2;
+// console.log(str);
+
+// console.log("1" + 2);   // 12
+// console.log(1 + "2");   // 12
+// console.log("1" + 2 + 2);   // 122
+// console.log(1 + "2" + 1);  //121
+// console.log(1 + 2 + "2");  // 32
+// console.log(1 + 2 + "2" + 1);  //321
+
+// console.log(true);  // true
+// console.log(+true); // 1
+// console.log(+"");   // 0
+
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2;  // DONT
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);   // 101
+++gameCounter
+console.log(gameCounter);   // 102
 

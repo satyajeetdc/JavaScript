732ad788d84c1d3ac1551b79080025705535c137
Satyajeet Das Chowdhury <51711970+satyajeetdc@users.noreply.github.com>
2024-04-16 03:57:16 +0000

    numbers

---
+++ 01_basics/06_numsAndMath.js
@@ -0,0 +1,18 @@
+// const accountBalance = 100;
+// const balance = new Number(100);
+
+// console.log(accountBalance);    // 100
+// console.log(balance);   // [Number: 100]
+
+// console.log(balance.toString().length)   // 3
+
+// console.log(balance.toFixed(2));    // 100.00
+
+// const num = 23.36654;
+// console.log(num.toPrecision(4));    // 23.37
+// console.log(num.toPrecision(100));  // LIMIT -> 1 to 100
+
+// const number = 1000000;
+// console.log(number.toLocaleString("en-IN"));
+
+

9fd7ddf0e81b5deff0b3cae61670d88a3bf4eb59
Satyajeet Das Chowdhury <51711970+satyajeetdc@users.noreply.github.com>
2024-04-17 03:27:57 +0000

    edit

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -7,3 +7,29 @@ const sumOfArray = myNums.reduce(
 );
 
 console.log(sumOfArray);
+
+
+// Shopping cart object
+
+const shoppingCart = [
+  {
+    itemName: "js course",
+    price: 2999,
+  },
+  {
+    itemName: "py course",
+    price: 999,
+  },
+  {
+    itemName: "mobile dev course",
+    price: 5999,
+  },
+  {
+    itemName: "data science course",
+    price: 12999,
+  },
+];
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0);
+
+console.log(priceToPay);
